d87b211cb0fb3ad1e29bd31a15e49935395f47b9
Jersim Tugonon <jersimtugonon83@gmail.com>
2026-05-29 11:18:33 +0800

    Update 1_Basic_Structure.c

--- C/1_Basic_Structure.c
+++ C/1_Basic_Structure.c
@@ -11,7 +11,9 @@ int main(){
    example of
    multi line comment
    */
-   
+
+   printf("\t1\t2\t3 \n");  // \t this simbol means escape it add space
+   printf("\n\t4\t5\t6 \n"); // \n this one is new line
 
    printf("\n Welcome to c programming \n");
    
