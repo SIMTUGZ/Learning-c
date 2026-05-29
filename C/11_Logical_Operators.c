14968a5f8fa0f37e38445a7dd2da4ad9e990cbda
Jersim Tugonon <jersimtugonon83@gmail.com>
2026-05-30 07:32:41 +0800

    Create 11_Logical_Operators.c

---
+++ C/11_Logical_Operators.c
@@ -0,0 +1,8 @@
+//_Logical_Operators
+
+//Logical operators are used to combine and evaluate conditions. 
+//They are commonly used in decision-making statements and loops.
+
+// && this and both of them must need to be true
+// || this logical operator or one of them must need to be true
+// ! (NOT) operator returns the opposite boolean value (true → false, false → true)
