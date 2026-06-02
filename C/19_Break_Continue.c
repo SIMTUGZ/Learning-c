746e48e4f0d05fd01e0bdd44622979e978e0c387
Jersim Tugonon <jersimtugonon83@gmail.com>
2026-06-03 00:14:26 +0800

    Create 19_Break_Continue.c

---
+++ C/19_Break_Continue.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// Break and Continue
+
+int main(){
+    
+    for(int i = 0; i < 10; i++){
+        
+        
+        printf("%d \n",i);
+        
+        // break; the use of this break command to stop the loop 
+        /*
+        if(i > 5){
+            break;
+        }
+        */
+         
+        /*
+        if(i == 5){
+            continue; //the use of continue command will skip the loop
+        }
+        */
+        
+        
+    }
+    
+    return 0;
+}
