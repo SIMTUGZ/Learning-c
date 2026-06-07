8ac42cd3d8aae71c5cd5a8be68781faceb82b660
Jersim Tugonon <jersimtugonon83@gmail.com>
2026-06-08 00:05:25 +0800

    Create 22_Array.c

---
+++ C/22_Array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+// A Array is a multiple collection of elements just like a varaiable but it can store more
+
+int main(){
+    
+    //basic example of Array
+    // 0  1  2   3   4  this is call elements // 0  1  2  3   4  <- These are called "indexes" (the positions)
+    int numbers[] = {10 ,9 ,8 ,7 , 6};
+    char ch[] = {'A','B','C','D'};
+    char name[] = "SIMTUGZ"; //this string is a array of character each character is represent as a element
+    
+    int size = sizeof(numbers);// sizeof command it will count how many elements of the array or how many bites in the array 
+    
+    printf("%d \n",size); 
+    
+    
+    printf("%d \n",numbers[3]);
+    printf("%c \n",ch[3]);
+    printf("%c \n",name[3]);
+    
+    //ok now lets try to use loops 
+    
+    for(int i = 0 ; i < 5; i++){
+        printf("%d ", numbers[i]); //this will update the loop 
+    }
+    
+    return 0;
+}
