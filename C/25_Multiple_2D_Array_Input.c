8b68cb1be9166c9a8f6c8cc7a43ce732f3f25db4
Jersim Tugonon <jersimtugonon83@gmail.com>
2026-06-08 15:53:07 +0800

    Create 25_Multiple_2D_Array_Input.c

---
+++ C/25_Multiple_2D_Array_Input.c
@@ -0,0 +1,28 @@
+//This is example of multiple input of 2d Array
+
+#include<stdio.h>
+
+int main(){
+    
+    int arr[2][4];
+    
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 4; j++){
+            printf("Input array[%d][%d]: ",i,j);
+            scanf("%d",& arr[i][j]);
+            
+        }
+    }
+    
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 4; j++){
+            printf("%d",arr[i][j]);
+            
+        }
+        printf("\n");
+    }
+    
+    
+    
+    return 0;
+}
