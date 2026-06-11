9e6376fc54d51dc6ee76891febb1521b9f544339
Jersim Tugonon <jersimtugonon83@gmail.com>
2026-06-11 14:37:55 +0800

    Update 27_Multiple_3D_Array_Input.c

--- C/27_Multiple_3D_Array_Input.c
+++ C/27_Multiple_3D_Array_Input.c
@@ -8,9 +8,9 @@ int main(){
     int arr[2][2][3]; // depth, row and column
 
     // input values using nested loops
-    for(int k = 0; k < 2; k++){
-        for(int i = 0; i < 2; i++){
-            for(int j = 0; j < 3; j++){
+    for(int k = 0; k < 2; k++){ //box
+        for(int i = 0; i < 2; i++){ //row
+            for(int j = 0; j < 3; j++){ //collumn
                 printf("Input arr[%d][%d][%d]: ", k, i, j);
                 scanf("%d", &arr[k][i][j]);
             }
