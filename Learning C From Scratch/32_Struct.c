aae5027fae4ccb5aa4f04511fbc0c466392cfde8
Jersim Tugonon <jersimtugonon83@gmail.com>
2026-06-13 01:46:12 +0800

    Update 32_Struct.c

--- Learning C From Scratch/32_Struct.c
+++ Learning C From Scratch/32_Struct.c
@@ -80,3 +80,60 @@ User1.gpa
 Use struct when multiple variables belong
 to the same object or record.
 */
+
+
+
+Aditional Information example
+
+
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+
+typedef struct{ // we can use typedef to make the code less messy
+    char name[100];
+    int age;
+    float gpa;
+    bool isWorker;
+}Worker;
+
+void StringPrint(Worker person1);// Prototype funcion
+
+int main(){
+    
+    Worker person1 = {"jersim",19,1.0,true};
+    Worker person2 = {"Tugonon",19,3.0,false};
+    
+    printf("name: %s \n",person1.name);
+    printf("name: %s \n",person2.name);
+    printf("age: %d \n",person1.age);
+    printf("age: %d \n",person2.age);
+    
+    person1.isWorker == 1? printf("true\n") : printf("false\n");
+    person2.isWorker == 1? printf("true\n") : printf("false\n");
+    
+    printf("%.1f \n",person1.gpa);
+    printf("%.1f \n",person2.gpa);
+    
+    
+    // manual adding data in the code
+    
+    //string using string copy or strcpy
+    strcpy(person1.name,"this is me \n");
+    printf("%s",person1.name);
+    
+    //adding data type in c be like
+    person2.age = 12;
+    printf("person2 age: %d",person2.age);
+    
+    
+    StringPrint( person1); // transfer in the other fuction
+    
+    return 0;
+}
+
+
+void StringPrint(Worker person1){ //fuction
+    printf("name: %s \n",person1.name);
+    printf("age: %d \n",person1.age);
+}
